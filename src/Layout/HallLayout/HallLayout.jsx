fb7faa5ae49fcc24bc34732dc46ba13371f0a39d
Denis Pozder <denispozder6@gmail.comm>
2023-12-15 23:27:00 +0100

    Saving Changes

--- src/Layout/HallLayout/HallLayout.jsx
+++ src/Layout/HallLayout/HallLayout.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import HallNavbar from '../HallNavbar/HallNavbar'
 import HallFooter from '../HallFooter/HallFooter'
+import { IoFastFood } from "react-icons/io5";
+import './hall-layout.css'
+import { Link } from 'react-router-dom';
 
 const HallLayout = ({children}) => {
   return (
@@ -8,6 +11,10 @@ const HallLayout = ({children}) => {
         <HallNavbar />
         {children}
         <HallFooter />
+        <Link to='/restoran/početna' className="hl-restaurant">
+          <IoFastFood />
+          <p>Restoran</p>
+        </Link>
     </div>
   )
 }
