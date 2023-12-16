72b8eb67001abcda40d58e72001d20eed230f96c
Denis Pozder <denispozder6@gmail.comm>
2023-12-16 19:48:26 +0100

    Saving Changes

--- src/Pages/Gameroom/Components/GameroomComponent/GameroomComponent.jsx
+++ src/Pages/Gameroom/Components/GameroomComponent/GameroomComponent.jsx
@@ -7,7 +7,7 @@ const GameroomComponent = () => {
     <div className="gameroom-component">
         <div className="grc-content">
             <div className="grc-column slide-in from-left">
-                <h3>Dobrodošli u </h3>
+                <h3>Dobrodošli u naše posebno igralište koje je stvoreno sa ciljem da pruži deci nezaboravo iskustvo zabave, učenja i smeha. Smešteno unutar našeg restorana, ovo igralište je dizajnirano kako bi pružilo siguran i stimulativan prostor gde se mališani mogu slobodno izražavati i istraživati.</h3>
             </div>
             <div className="grc-column slide-in from-right">
                 <img src={GameroomImg} alt="Slika igraonice" />
