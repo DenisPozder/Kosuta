078488f75cac980a292ec691cb5b0b9c4d820aff
Denis Pozder <denispozder6@gmail.comm>
2023-12-18 01:06:50 +0100

    Saving Changes

--- src/HallData/HallGalleryData.js
+++ src/HallData/HallGalleryData.js
@@ -1,4 +1,56 @@
 import hallImg from '../Assets/Hall/hall1.jpg'
+import kamin1 from '../Assets/Hall/kamin1.jpg'
+import kamin2 from '../Assets/Hall/kamin2.jpg'
+import kamin3 from '../Assets/Hall/kamin3.jpg'
+import kamin4 from '../Assets/Hall/kamin4.jpg'
+import kamin5 from '../Assets/Hall/kamin5.jpg'
+import kamin6 from '../Assets/Hall/kamin6.jpg'
+import kamin7 from '../Assets/Hall/kamin7.jpg'
+import kamin8 from '../Assets/Hall/kamin8.jpg'
+import kamin9 from '../Assets/Hall/kamin9.jpg'
+import kamin10 from '../Assets/Hall/kamin10.jpg'
+import kamin11 from '../Assets/Hall/kamin11.jpg'
+import kamin12 from '../Assets/Hall/kamin12.jpg'
+import kamin13 from '../Assets/Hall/kamin13.jpg'
+import kamin14 from '../Assets/Hall/kamin14.jpg'
+import kamin15 from '../Assets/Hall/kamin15.jpg'
+import kamin16 from '../Assets/Hall/kamin16.jpg'
+import kamin17 from '../Assets/Hall/kamin17.jpg'
+import kamin18 from '../Assets/Hall/kamin18.jpg'
+import kamin19 from '../Assets/Hall/kamin19.jpg'
+import kamin20 from '../Assets/Hall/kamin20.jpg'
+import kamin21 from '../Assets/Hall/kamin21.jpg'
+import kamin22 from '../Assets/Hall/kamin22.jpg'
+import kamin23 from '../Assets/Hall/kamin23.jpg'
+import kamin24 from '../Assets/Hall/kamin24.jpg'
+import kamin25 from '../Assets/Hall/kamin25.jpg'
+import kamin26 from '../Assets/Hall/kamin26.jpg'
+import kamin27 from '../Assets/Hall/kamin27.jpg'
+import kamin28 from '../Assets/Hall/kamin28.jpg'
+import kamin29 from '../Assets/Hall/kamin29.jpg'
+import kamin30 from '../Assets/Hall/kamin30.jpg'
+import kamin31 from '../Assets/Hall/kamin31.jpg'
+import kamin32 from '../Assets/Hall/kamin32.jpg'
+import kamin33 from '../Assets/Hall/kamin33.jpg'
+import kamin34 from '../Assets/Hall/kamin34.jpg'
+import kamin35 from '../Assets/Hall/kamin35.jpg'
+import kamin36 from '../Assets/Hall/kamin36.jpg'
+import kamin37 from '../Assets/Hall/kamin37.jpg'
+import kamin38 from '../Assets/Hall/kamin38.jpg'
+import kamin39 from '../Assets/Hall/kamin39.jpg'
+import kamin40 from '../Assets/Hall/kamin40.jpg'
+import kamin41 from '../Assets/Hall/kamin41.jpg'
+import kamin42 from '../Assets/Hall/kamin42.jpg'
+import kamin43 from '../Assets/Hall/kamin43.jpg'
+import kamin44 from '../Assets/Hall/kamin44.jpg'
+import kamin45 from '../Assets/Hall/kamin45.jpg'
+import kamin46 from '../Assets/Hall/kamin46.jpg'
+import kamin47 from '../Assets/Hall/kamin47.jpg'
+import kamin48 from '../Assets/Hall/kamin48.jpg'
+import kamin49 from '../Assets/Hall/kamin49.jpg'
+import kamin50 from '../Assets/Hall/kamin50.jpg'
+import kamin51 from '../Assets/Hall/kamin51.jpg'
+import kamin52 from '../Assets/Hall/kamin52.jpg'
 
 export const HallGalleryData = [
     {
@@ -50,19 +102,211 @@ export const HallGalleryData = [
         category: "svečana"
     },
     {
-        image: hallImg,
+        image: kamin1,
         category: "kamin"
     },
     {
-        image: hallImg,
+        image: kamin2,
         category: "kamin"
     },
     {
-        image: hallImg,
+        image: kamin3,
         category: "kamin"
     },
     {
-        image: hallImg,
+        image: kamin4,
+        category: "kamin"
+    },
+    {
+        image: kamin5,
+        category: "kamin"
+    },
+    {
+        image: kamin6,
+        category: "kamin"
+    },
+    {
+        image: kamin7,
+        category: "kamin"
+    },
+    {
+        image: kamin8,
+        category: "kamin"
+    },
+    {
+        image: kamin9,
+        category: "kamin"
+    },
+    {
+        image: kamin10,
+        category: "kamin"
+    },
+    {
+        image: kamin11,
+        category: "kamin"
+    },
+    {
+        image: kamin12,
+        category: "kamin"
+    },
+    {
+        image: kamin13,
+        category: "kamin"
+    },
+    {
+        image: kamin14,
+        category: "kamin"
+    },
+    {
+        image: kamin15,
+        category: "kamin"
+    },
+    {
+        image: kamin16,
+        category: "kamin"
+    },
+    {
+        image: kamin17,
+        category: "kamin"
+    },
+    {
+        image: kamin18,
+        category: "kamin"
+    },
+    {
+        image: kamin19,
+        category: "kamin"
+    },
+    {
+        image: kamin20,
+        category: "kamin"
+    },
+    {
+        image: kamin21,
+        category: "kamin"
+    },
+    {
+        image: kamin22,
+        category: "kamin"
+    },
+    {
+        image: kamin23,
+        category: "kamin"
+    },
+    {
+        image: kamin24,
+        category: "kamin"
+    },
+    {
+        image: kamin25,
+        category: "kamin"
+    },
+    {
+        image: kamin26,
+        category: "kamin"
+    },
+    {
+        image: kamin27,
+        category: "kamin"
+    },
+    {
+        image: kamin28,
+        category: "kamin"
+    },
+    {
+        image: kamin29,
+        category: "kamin"
+    },
+    {
+        image: kamin30,
+        category: "kamin"
+    },
+    {
+        image: kamin31,
+        category: "kamin"
+    },
+    {
+        image: kamin32,
+        category: "kamin"
+    },
+    {
+        image: kamin33,
+        category: "kamin"
+    },
+    {
+        image: kamin34,
+        category: "kamin"
+    },
+    {
+        image: kamin35,
+        category: "kamin"
+    },
+    {
+        image: kamin36,
+        category: "kamin"
+    },
+    {
+        image: kamin37,
+        category: "kamin"
+    },
+    {
+        image: kamin38,
+        category: "kamin"
+    },
+    {
+        image: kamin39,
+        category: "kamin"
+    },
+    {
+        image: kamin40,
+        category: "kamin"
+    },
+    {
+        image: kamin41,
+        category: "kamin"
+    },
+    {
+        image: kamin42,
+        category: "kamin"
+    },
+    {
+        image: kamin43,
+        category: "kamin"
+    },
+    {
+        image: kamin44,
+        category: "kamin"
+    },
+    {
+        image: kamin45,
+        category: "kamin"
+    },
+    {
+        image: kamin46,
+        category: "kamin"
+    },
+    {
+        image: kamin47,
+        category: "kamin"
+    },
+    {
+        image: kamin48,
+        category: "kamin"
+    },
+    {
+        image: kamin49,
+        category: "kamin"
+    },
+    {
+        image: kamin50,
+        category: "kamin"
+    },
+    {
+        image: kamin51,
+        category: "kamin"
+    },
+    {
+        image: kamin52,
         category: "kamin"
     },
 ]
