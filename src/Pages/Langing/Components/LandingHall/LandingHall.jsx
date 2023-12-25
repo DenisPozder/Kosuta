a49837aa788a6f109bbe5e420aa53ee6bf4ed4b8
Denis Pozder <denispozder6@gmail.comm>
2023-12-25 15:26:51 +0100

    Saving Changes

--- src/Pages/Langing/Components/LandingHall/LandingHall.jsx
+++ src/Pages/Langing/Components/LandingHall/LandingHall.jsx
@@ -10,52 +10,11 @@ const LandingHall = () => {
 
   const { i18n } = useTranslation('')
 
-  useEffect(() => {
-    const landingHall = document.querySelector('.landing-hall')
-    const parallax_ele = document.querySelectorAll('.parallax-hall')
-    let xValueh = 0,
-        yValueh = 0
-
-    let rotateDegreeh = 0
-
-    function updateHall (cursorPosition) {
-        parallax_ele.forEach((el) => {
-            let speedxh = el.dataset.speedx
-            let speedyh = el.dataset.speedy
-            let speedzh = el.dataset.speedz
-            let rotationh = el.dataset.rotation
-
-            let isInLefth = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1
-            let zValueh = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLefth * 0.1
-
-            el.style.transform = `translateX(calc(-50% + ${-xValueh * speedxh}px))
-            translateY(calc(-50% + ${yValueh * speedyh}px))
-            rotateY(${rotateDegreeh * rotationh}deg)
-            perspective(2300px)
-            translateZ(${zValueh * speedzh}px)`
-        })
-    }
-
-    updateHall(0)
-
-    landingHall.addEventListener('mousemove', (e) => {
-        xValueh = e.clientX - window.innerWidth / 2
-        yValueh = e.clientY - window.innerHeight / 2
-
-        rotateDegreeh = (xValueh / (window.innerWidth / 2)) * 20
-
-        updateHall(e.clientX)
-    })
-  },[])
-
   return (
     <Link to="/početna" className="landing-hall">
       <img className='lh-main-img' src={hallImg} alt="Slika sala" />
       <div className="lh-main-overlay"></div>
-      <div className="lh-table parallax-hall" data-speedx="0.009" data-speedy="0.09" data-speedz="0.50" data-rotation="0.2">
-        <img src={hallTable} alt="Dekorativna slika" />
-      </div>
-      <h1 className='lh-text parallax-hall' data-speedx="0.05" data-speedy="0.05" data-speedz="0" data-rotation="0">Košuta <span>{i18n.language === 'sr' ? 'Proslave' : 'Celebrations'}</span></h1>
+      <h1 className='lh-text parallax-hall'>Košuta <span>{i18n.language === 'sr' ? 'Proslave' : 'Celebrations'}</span></h1>
       <div className='lh-button-content'>
         <Link className='lh-button' to={'/početna'}><h3>{i18n.language === 'sr' ? 'posetite sale' : 'visit halls'}</h3></Link>
       </div>
--- src/Pages/Langing/Components/LandingRestaurant/LandingRestaurant.jsx
+++ src/Pages/Langing/Components/LandingRestaurant/LandingRestaurant.jsx
@@ -1,11 +1,6 @@
 import React, { useEffect } from 'react'
 import './landing-restaurant.css'
 import { Link } from 'react-router-dom'
-import restaurantLeaves from '../../../../Assets/Landing/topLeaves.png'
-import restaurantTable from '../../../../Assets/Landing/restaurantTable.png'
-import animalImg from '../../../../Assets/Landing/animal.png'
-import plants from '../../../../Assets/Landing/plants.png'
-import restaurantTree from '../../../../Assets/Landing/restaurantTree.png'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { useTranslation } from 'react-i18next'
 import restaurantImg from '../../../../Assets/Restaurant/restaurantImage.jpg'
@@ -14,69 +9,16 @@ const LandingRestaurant = () => {
 
     const { i18n } = useTranslation('')
 
-    useEffect(() => {
-        const landingRestaurant = document.querySelector('.landing-restaurant')
-        const parallax_el = document.querySelectorAll('.parallax')
-        let xValue = 0,
-            yValue = 0
-
-        let rotateDegree = 0
-
-        function update (cursorPosition) {
-            parallax_el.forEach((el) => {
-                let speedx = el.dataset.speedx
-                let speedy = el.dataset.speedy
-                let speedz = el.dataset.speedz
-                let rotation = el.dataset.rotation
-
-                let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1
-                let zValue = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1
-
-                el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px))
-                translateY(calc(-50% + ${yValue * speedy}px))
-                rotateY(${rotateDegree * rotation}deg)
-                perspective(2300px)
-                translateZ(${zValue * speedz}px)`
-            })
-        }
-
-        update(0)
-
-        landingRestaurant.addEventListener('mousemove', (e) => {
-            xValue = e.clientX - window.innerWidth / 2
-            yValue = e.clientY - window.innerHeight / 2
-
-            rotateDegree = (xValue / (window.innerWidth / 2)) * 20
-
-            update(e.clientX)
-        })
-    },[])
-
   return (
     <Link to='/restoran/početna' className="landing-restaurant">
         <div className="vignette"></div>
         <div className="lp-top-overlay"></div>
         <div className="lp-bottom-overlay"></div>
         <div className="lr-main-overlay"></div>
-        <img className='lr-main-img' src={restaurantImg} alt="Slika restorana" />
-        <div className="lr-leaves parallax" data-speedx="0.10" data-speedy="0.10" data-speedz="0" data-rotation="0">
-            <LazyLoadImage src={restaurantLeaves} alt="Dekorativna slika" />
-        </div>
-        <div className="lr-table parallax" data-speedx="0.05" data-speedy="0.05" data-speedz="0.70" data-rotation="0">
-            <LazyLoadImage src={restaurantTable} alt="Dekorativna slika" />
-        </div>
-        <h1 className='lr-text parallax' data-speedx="0.05" data-speedy="0.05" data-speedz="0" data-rotation="0">Košuta <span>{i18n.language === 'sr' ? 'Restoran' : "Restaurant"}</span></h1>
+        <LazyLoadImage className='lr-main-img' src={restaurantImg} alt="Slika restorana" />
+        <h1 className='lr-text parallax'>Košuta <span>{i18n.language === 'sr' ? 'Restoran' : "Restaurant"}</span></h1>
         <div className='lr-button-content'>
             <Link className='lr-button' to={'/restoran/početna'}><h3>{i18n.language === 'sr' ? 'Posetite restoran' : 'Visit restaurant'}</h3></Link>
-        </div>
-        <div className="lr-animal parallax" data-speedx="0.03" data-speedy="0.03" data-speedz="0.80" data-rotation="0.18">
-            <LazyLoadImage src={animalImg} alt="Dekorativna slika" />
-        </div>
-        <div className='lr-plants parallax' data-speedx="0.009" data-speedy="0.09" data-speedz="0.50" data-rotation="0.2">
-            <img src={plants} alt="Dekorativna slika" />
-        </div>
-        <div className="lr-tree parallax" data-speedx="0.007" data-speedy="0.07" data-speedz="0.40" data-rotation="0.1">
-            <LazyLoadImage src={restaurantTree} alt="Dekorativna slika" />
         </div>
     </Link>
   )
